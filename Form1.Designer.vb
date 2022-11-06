e1d7f995c1eacd870c182acf5f067369f746c134
Faizalmuhamadiqbal27 <117532465+Faizalmuhamadiqbal27@users.noreply.github.com>
2022-11-06 04:12:54 -0800

    Coding dan Desain dari Form Nilai Mahasiswa
    Nama : Faizal Muhamad Iqbal
    Npm : 2021804004
    Disini saya membuat satu tugas tentang Form penilaian mahasiswa yang mana desain dan coding saya buat pribadi

---
+++ Form1.Designer.vb
@@ -0,0 +1,401 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape3 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape2 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.OvalShape1 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.npm = New System.Windows.Forms.Label()
+        Me.nama = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.nilaitugas = New System.Windows.Forms.Label()
+        Me.nilaiuts = New System.Windows.Forms.Label()
+        Me.nilaiuas = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ips = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.ipk = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.sks = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.nilaibobot = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.totalsemester = New System.Windows.Forms.Label()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Kristen ITC", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label1.Location = New System.Drawing.Point(219, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(332, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "FORM NILAI MAHASISWA"
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape3, Me.LineShape2, Me.LineShape1, Me.OvalShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(810, 431)
+        Me.ShapeContainer1.TabIndex = 1
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape3
+        '
+        Me.LineShape3.Name = "LineShape3"
+        Me.LineShape3.X1 = 13
+        Me.LineShape3.X2 = 780
+        Me.LineShape3.Y1 = 280
+        Me.LineShape3.Y2 = 280
+        '
+        'LineShape2
+        '
+        Me.LineShape2.Name = "LineShape2"
+        Me.LineShape2.X1 = 16
+        Me.LineShape2.X2 = 482
+        Me.LineShape2.Y1 = 178
+        Me.LineShape2.Y2 = 179
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 16
+        Me.LineShape1.X2 = 481
+        Me.LineShape1.Y1 = 122
+        Me.LineShape1.Y2 = 122
+        '
+        'OvalShape1
+        '
+        Me.OvalShape1.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.OvalShape1.BorderColor = System.Drawing.SystemColors.InactiveCaption
+        Me.OvalShape1.Location = New System.Drawing.Point(194, 5)
+        Me.OvalShape1.Name = "OvalShape1"
+        Me.OvalShape1.Size = New System.Drawing.Size(378, 66)
+        '
+        'npm
+        '
+        Me.npm.AutoSize = True
+        Me.npm.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.npm.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.npm.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.npm.Location = New System.Drawing.Point(12, 93)
+        Me.npm.Name = "npm"
+        Me.npm.Size = New System.Drawing.Size(43, 20)
+        Me.npm.TabIndex = 2
+        Me.npm.Text = "NPM"
+        '
+        'nama
+        '
+        Me.nama.AutoSize = True
+        Me.nama.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.nama.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nama.Location = New System.Drawing.Point(12, 138)
+        Me.nama.Name = "nama"
+        Me.nama.Size = New System.Drawing.Size(55, 20)
+        Me.nama.TabIndex = 3
+        Me.nama.Text = "NAMA"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.ForeColor = System.Drawing.Color.Crimson
+        Me.TextBox1.Location = New System.Drawing.Point(100, 93)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(382, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.ForeColor = System.Drawing.Color.Crimson
+        Me.TextBox2.Location = New System.Drawing.Point(100, 138)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(382, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'nilaitugas
+        '
+        Me.nilaitugas.AutoSize = True
+        Me.nilaitugas.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.nilaitugas.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nilaitugas.Location = New System.Drawing.Point(12, 196)
+        Me.nilaitugas.Name = "nilaitugas"
+        Me.nilaitugas.Size = New System.Drawing.Size(110, 20)
+        Me.nilaitugas.TabIndex = 6
+        Me.nilaitugas.Text = "NILAI TUGAS"
+        '
+        'nilaiuts
+        '
+        Me.nilaiuts.AutoSize = True
+        Me.nilaiuts.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.nilaiuts.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nilaiuts.Location = New System.Drawing.Point(253, 196)
+        Me.nilaiuts.Name = "nilaiuts"
+        Me.nilaiuts.Size = New System.Drawing.Size(86, 20)
+        Me.nilaiuts.TabIndex = 7
+        Me.nilaiuts.Text = "NILAI UTS"
+        '
+        'nilaiuas
+        '
+        Me.nilaiuas.AutoSize = True
+        Me.nilaiuas.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.nilaiuas.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nilaiuas.Location = New System.Drawing.Point(508, 196)
+        Me.nilaiuas.Name = "nilaiuas"
+        Me.nilaiuas.Size = New System.Drawing.Size(88, 20)
+        Me.nilaiuas.TabIndex = 8
+        Me.nilaiuas.Text = "NILAI UAS"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.ForeColor = System.Drawing.Color.Crimson
+        Me.TextBox3.Location = New System.Drawing.Point(128, 196)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox3.TabIndex = 9
+        '
+        'TextBox4
+        '
+        Me.TextBox4.ForeColor = System.Drawing.Color.Crimson
+        Me.TextBox4.Location = New System.Drawing.Point(367, 196)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox4.TabIndex = 10
+        '
+        'TextBox5
+        '
+        Me.TextBox5.ForeColor = System.Drawing.Color.Crimson
+        Me.TextBox5.Location = New System.Drawing.Point(664, 196)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox5.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.AppStarting
+        Me.Button1.Location = New System.Drawing.Point(16, 292)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "CLEAR"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.AppStarting
+        Me.Button2.Location = New System.Drawing.Point(111, 292)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "HITUNG"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'ips
+        '
+        Me.ips.AutoSize = True
+        Me.ips.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ips.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ips.Location = New System.Drawing.Point(87, 338)
+        Me.ips.Name = "ips"
+        Me.ips.Size = New System.Drawing.Size(35, 20)
+        Me.ips.TabIndex = 14
+        Me.ips.Text = "IPS"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextBox6.Location = New System.Drawing.Point(128, 338)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ReadOnly = True
+        Me.TextBox6.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox6.TabIndex = 15
+        '
+        'ipk
+        '
+        Me.ipk.AutoSize = True
+        Me.ipk.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ipk.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ipk.Location = New System.Drawing.Point(317, 338)
+        Me.ipk.Name = "ipk"
+        Me.ipk.Size = New System.Drawing.Size(34, 20)
+        Me.ipk.TabIndex = 16
+        Me.ipk.Text = "IPK"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextBox7.Location = New System.Drawing.Point(368, 338)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.ReadOnly = True
+        Me.TextBox7.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox7.TabIndex = 17
+        '
+        'sks
+        '
+        Me.sks.AutoSize = True
+        Me.sks.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.sks.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.sks.Location = New System.Drawing.Point(81, 243)
+        Me.sks.Name = "sks"
+        Me.sks.Size = New System.Drawing.Size(41, 20)
+        Me.sks.TabIndex = 18
+        Me.sks.Text = "SKS"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.ForeColor = System.Drawing.Color.Crimson
+        Me.TextBox8.Location = New System.Drawing.Point(128, 243)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox8.TabIndex = 19
+        '
+        'nilaibobot
+        '
+        Me.nilaibobot.AutoSize = True
+        Me.nilaibobot.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.nilaibobot.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nilaibobot.Location = New System.Drawing.Point(253, 243)
+        Me.nilaibobot.Name = "nilaibobot"
+        Me.nilaibobot.Size = New System.Drawing.Size(109, 20)
+        Me.nilaibobot.TabIndex = 20
+        Me.nilaibobot.Text = "NILAI BOBOT"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(368, 243)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.ReadOnly = True
+        Me.TextBox9.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox9.TabIndex = 21
+        '
+        'totalsemester
+        '
+        Me.totalsemester.AutoSize = True
+        Me.totalsemester.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.totalsemester.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.totalsemester.Location = New System.Drawing.Point(508, 243)
+        Me.totalsemester.Name = "totalsemester"
+        Me.totalsemester.Size = New System.Drawing.Size(152, 20)
+        Me.totalsemester.TabIndex = 22
+        Me.totalsemester.Text = "TOTAL SEMESTER"
+        '
+        'TextBox10
+        '
+        Me.TextBox10.ForeColor = System.Drawing.Color.Crimson
+        Me.TextBox10.Location = New System.Drawing.Point(664, 243)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox10.TabIndex = 23
+        '
+        'TextBox11
+        '
+        Me.TextBox11.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextBox11.Location = New System.Drawing.Point(197, 382)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.ReadOnly = True
+        Me.TextBox11.Size = New System.Drawing.Size(186, 20)
+        Me.TextBox11.TabIndex = 24
+        Me.TextBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(810, 431)
+        Me.Controls.Add(Me.TextBox11)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.totalsemester)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.nilaibobot)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.sks)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.ipk)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.ips)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.nilaiuas)
+        Me.Controls.Add(Me.nilaiuts)
+        Me.Controls.Add(Me.nilaitugas)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.nama)
+        Me.Controls.Add(Me.npm)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape3 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape2 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents OvalShape1 As Microsoft.VisualBasic.PowerPacks.OvalShape
+    Friend WithEvents npm As System.Windows.Forms.Label
+    Friend WithEvents nama As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents nilaitugas As System.Windows.Forms.Label
+    Friend WithEvents nilaiuts As System.Windows.Forms.Label
+    Friend WithEvents nilaiuas As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ips As System.Windows.Forms.Label
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents ipk As System.Windows.Forms.Label
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents sks As System.Windows.Forms.Label
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents nilaibobot As System.Windows.Forms.Label
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents totalsemester As System.Windows.Forms.Label
+    Friend WithEvents TextBox10 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox11 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Menghapus semua isi huruf dan angka
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+        TextBox9.Text = ""
+        TextBox10.Text = ""
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Memberi Inisial pada masing-masing variabel/objek
+        Dim npm = TextBox1.Text
+        Dim NAMA = TextBox2.Text
+        Dim sks = TextBox8.Text
+        Dim NILAITUGAS As Double = TextBox3.Text
+        Dim Nilaiuas As Double = TextBox5.Text
+        Dim nilaiuts As Double = TextBox4.Text
+        Dim nilaibobot = TextBox9.Text
+        Dim totalsemester = TextBox10.Text
+        Dim ips
+        Dim ipk
+
+        'Menghitung nilai bobot(nilaitugas*20% + nilaiuts*30% + nilaiuas*50%
+        nilaibobot = (NILAITUGAS * 0.2 + Nilaiuas * 0.5 + nilaiuts * 0.3)
+        TextBox9.Text = nilaibobot
+
+        'Menghitung nilai IPS (nilai bobot/sks)
+        ips = (nilaibobot / sks)
+        TextBox6.Text = ips
+
+        'Menghitung nilai ipk(nilai bobot*totalsemester/sks*totalsemester)
+        ipk = ((nilaibobot * totalsemester) / (sks * totalsemester))
+        TextBox7.Text = ipk
+        TextBox6.Text = TextBox6.Text.Substring(0, 3)
+        TextBox7.Text = TextBox7.Text.Substring(0, 3)
+
+        'Apabila IPK lebih dari 3, Maka Dinyatakan Lulus
+        If TextBox7.Text >= "3" Then
+            TextBox11.Text = "SELAMAT ANDA LULUS"
+        End If
+    End Sub
+End Class
